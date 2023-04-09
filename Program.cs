2720aad1a8ce73b57ccc3736bf7d117f26e44782
ALLAN SEBASTIAN QUINTANA ABARCA <allan.quintana@gmail.com>
2023-04-09 18:08:41 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class SumaSinOperador
+{
+    static void Main(string[] args)
+    {
+        int a;
+        int b;
+        int suma = 0;
+
+        Console.WriteLine("escribe el primer numero: ");
+        a=int.Parse(Console.ReadLine());
+        Console.WriteLine("escribe el segundo numero: ");
+        b=int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < a; i++)
+        {
+            suma++;
+        }
+
+        for (int i = 0; i < b; i++)
+        {
+            suma++;
+        }
+
+        Console.WriteLine("La suma de {0} y {1} es: {2}", a, b, suma);
+    }
+}
+
+
